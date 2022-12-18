ae05ac2d9b3e1ee42ce1299ff47ee96a9c789b2b
Erbay Aşkın <36074678+erbayaskin@users.noreply.github.com>
2022-12-18 23:16:28 +0300

    ilk
    ilk

---
+++ src/main/java/com/erbay/parkmatik/ParkmatikApplication.java
@@ -0,0 +1,13 @@
+package com.erbay.parkmatik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkmatikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkmatikApplication.class, args);
+	}
+
+}
