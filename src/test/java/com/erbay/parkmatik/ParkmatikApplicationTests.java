b53c0cf8448c66411f73b069840d7d55d6c41848
Erbay Aşkın <36074678+erbayaskin@users.noreply.github.com>
2022-12-18 23:19:17 +0300

    ilk
    ilk

---
+++ src/test/java/com/erbay/parkmatik/ParkmatikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erbay.parkmatik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkmatikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
